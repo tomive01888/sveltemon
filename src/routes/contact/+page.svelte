cc3881e342ab712ce5ddc34edcc49a8914d4ec68
petterkoding <69626923+petterkoding@users.noreply.github.com>
2024-04-28 13:56:25 +0200

    test call

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,5 +1,14 @@
 <script>
   let name = "lols";
+  const KANTO_REGION = "kanto";
+
+  async function getDex() {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokedex/${KANTO_REGION}`);
+    const data = await request.json();
+    console.log(data);
+  }
+
+  const wut = getDex();
 </script>
 
 <h1>Hello from {name}</h1>
