a1d3e2ef88e449eb7f76a4205a04c5669fb5aaf3
tomive_NOROFF <152199718+tomive01888@users.noreply.github.com>
2024-05-02 16:33:49 +0200

    rr

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,7 +5,7 @@
   async function getDex() {
     const request = await fetch(`https://pokeapi.co/api/v2/pokedex/${KANTO_REGION}`);
     const data = await request.json();
-    console.log("asdasdasd",data);
+    console.log("eeeeeeeeeeeeeeeee",data);
   }
 
   const wut = getDex();
